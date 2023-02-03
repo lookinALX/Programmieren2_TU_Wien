5f7aa9f74d1b397a63198a3645f44a752cbd28d7
lookinALX <67375219+lookinALX@users.noreply.github.com>
2023-02-03 10:00:38 +0100

    Add files via upload

--- Homework2/Homework2PartB/task.c
+++ Homework2/Homework2PartB/task.c
@@ -30,7 +30,36 @@ struct Vector *num_sample(const struct Vector *x, double (*func)(double))
 struct Vector *num_interpolate(const struct Vector *xinterp,
                                const struct Vector *x, const struct Vector *f)
 {
-  return NULL; // TODO: remove this placeholder and implement function here.
+  // y = y1 + (x-x1)(y2-y1)/(x2-x1)
+  struct Vector *res = vector_new();
+  vector_reserve(res, xinterp->size);
+  for(int i = 0; i < xinterp->size; i++){
+    if(xinterp->data[i] < x->data[0]){
+      res->data[i] = f->data[0];
+    }
+    else if (xinterp->data[i] > x->data[x->size - 1])
+    {
+      res->data[i] = f->data[f->size-1];
+    }
+    else{
+      double x1 = 0;
+      double x2 = 0;
+      double y1 = 0;
+      double y2 = 0;
+      for (int j = 0; j < x->size; j++)
+      {
+        if(x->data[j] > xinterp->data[i]){
+          x1 = x->data[j-1];
+          x2 = x->data[j];
+          y1 = f->data[j-1];
+          y2 = f->data[j];
+          res->data[i] = y1 + (xinterp->data[i] - x1)*(y2-y1)/(x2-x1);
+        }
+      }
+      
+    }
+  }
+  return res; // TODO: remove this placeholder and implement function here.
 }
 
 double func(double x)
@@ -50,13 +79,25 @@ int main()
 
   // TODO:
   // 1. sample 'func' for the 10 provided x-values ('x') using 'num_sample'.
+
+  struct Vector *f = num_sample(x, &func);
+
   // 2. interpolate function values for the 5 provided x-values ('xinterp')
   // using 'num_interpolate'.
+
+  struct Vector *fint= num_interpolate(xinterp,x, f);
+
   // 3. print the sampled function values and the interpolated function values
   // using 'vector_print'.
 
+  vector_print(f);
+  vector_print(fint);
+
   vector_delete(&x);
   vector_delete(&xinterp);
+
+  vector_delete(&f);
+  vector_delete(&fint);
 
   return 0;
 }
